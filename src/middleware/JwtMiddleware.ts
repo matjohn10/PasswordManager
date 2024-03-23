e0804eef7ed7aa256a257d8d7dc0a3d78ae7053b
Mathieu Johnson <mathieujohnson10@gmail.com>
2024-03-23 00:45:50 -0400

    Removed some useless console logs

--- src/middleware/JwtMiddleware.ts
+++ src/middleware/JwtMiddleware.ts
@@ -4,17 +4,14 @@ import { Request, Response, NextFunction } from "express";
 function authenticateToken(req: Request, res: Response, next: NextFunction) {
   const auth = req.headers["authorization"];
   const token = auth && auth.split(" ")[1];
-  console.log("no token");
   if (!token) {
     return res.status(401).json({ message: "Token not provided" });
   }
-  console.log("wrong token");
   jwt.verify(token, process.env.JWT_KEY, (err: any, user: any) => {
-    console.log("Middleware: ", token);
     if (err) return res.status(401).json({ message: "Invalid token", err });
 
+    console.log("Good user in middleware");
     req.body.user = user;
-    console.log(user);
     next();
   });
 }
